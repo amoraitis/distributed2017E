b1a89867b8f60aa4bf0cc662c26c7f11d030e95c
DESKTOP-GDHPB60\louks <loukspan@hotmail.com>
2017-04-08 19:05:06 +0300

    Implemented StringToHash methos

--- src/backendclient/AndroidClient.java
+++ src/backendclient/AndroidClient.java
@@ -1,6 +1,11 @@
 package backendclient;
 
+import workers.MapWorker;
 import workers.ReduceWorker;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 import master.Master;
 import model.*;
 // .abcdefghijklmnopqrstuvwxyz
@@ -8,7 +13,14 @@ public class AndroidClient {
 	public static void main(String[] args){
 		Master master = new Master();
 		//master.askGoogleDirectionsAPI("33.812092","-117.918974","34.138117","-118.353378");
-		new ReducerClient(922, 52).start();
+		//new ReducerClient(922, 52).start();
+		MapWorker mapWorker = new MapWorker();
+		try {
+			System.out.println(mapWorker.calculateHash("33.81-117.91"));
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println(mapWorker.getClass().getClassLoader().toString());
 		
 		
 	}
